c4cf31171e1e3fccbbf6db06728ed70d4e41a0e1
Furqon Muhammad Muflih <furonmm76@gmail.com>
2024-08-29 20:44:56 +0700

    update lagi

--- js/script.js
+++ js/script.js
@@ -25,3 +25,26 @@ function submitForm() {
         <p><strong>Pesan:</strong> ${message}</p>
     `;
 }
+
+
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('.banner-img');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === index) {
+            slide.classList.add('active');
+        }
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 5000); 
+showSlide(currentSlide);
